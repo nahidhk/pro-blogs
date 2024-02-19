e1f0eab533a99ecfdad2ae311de7557e7af836c2
nahidhk <nahidappltd@gmail.com>
2024-02-19 18:23:26 +0600

    add new

---
+++ js/app.js
@@ -0,0 +1,35 @@
+console.log("app.js");
+
+async function displayData() {
+    try {
+        const response = await fetch("/data/json/blogdata/myblogs.json");
+        const data = await response.json();
+        const dataContainer = document.getElementById('showmy-bata');
+
+        // Loop through the data and display it
+        data.forEach(item => {
+            const itemElement = document.createElement('div');
+            itemElement.innerHTML = `
+          <div class="cscard">
+
+      <img class="imgio" src="${item.imgurl}" />
+
+      <h1>${item.title}</h1>
+      <hr>
+      <p>
+      ${item.paragraph}
+      </p>
+      <a class="btn" href="${item.btnurl}" target="_blnk">Know more</a>
+    </div>  
+            `;
+            dataContainer.appendChild(itemElement);
+        });
+    } catch (error) {
+        console.error('Error fetching or displaying data:', error);
+    }
+}
+
+// Call the displayData function to load and display JSON data
+displayData();
+//
+//
